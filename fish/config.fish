7c77f83d12cec7037e7701d0c1e4c9ae53e6dbdf
babu <bguerrero825@gmail.com>
2025-08-14 16:52:48 -0500

    more dots!

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,3 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-set SHELL /usr/bin/fish; export SHELL
-set VIMRUNTIME /usr/local/share/nvim/runtime; export VIMRUNTIME
-set fish_greeting
