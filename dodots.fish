0dddb355d75afbb1d96ab9373d59e681bef2d7b1
babu <bguerrero825@gmail.com>
2025-08-15 11:23:19 -0500

    updated script

---
+++ dodots.fish
@@ -0,0 +1,23 @@
+#!/usr/bin/fish
+
+if $argv[1] == "push"
+	cd ~/dev/moredots
+	git fetch
+	git pull
+	rsync -avz ~/.tmux.conf ~/.config/kitty ~/.config/fish ~/dev/moredots ;
+	git add -u
+	git commit -m (hostname)": more dots!"
+	git push
+	echo "[+] dots pushed!"
+
+else if $argv[1] == "pull"
+	cd ~/dev/moredots
+	git fetch
+	git pull
+	rsync -avz ~/dev/moredots/.tmux.conf ~/.tmux.conf ;
+	rsync -avz ~/dev/moredots/fish ~/.config/fish ;
+	rsync -avz ~/dev/moredots/kitty ~/.config/kitty ;
+	echo "[+] dots pulled!"
+else
+	echo "Would you like to 'push' or 'pull' dots?"
+end
--- pushdots.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/fish
-
-cd ~/dev/moredots
-git pull
-rsync -avz ~/.tmux.conf ~/.config/kitty ~/.config/fish ~/dev/moredots ; rsync -avz ~/.config/nvim/init.lua ~/.config/nvim/lua ~/dev/moredots/nvim/ 
-git add -u
-git commit -m "more dots!"
-git push
