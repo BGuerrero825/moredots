666bbc991882d02f7b35d13638a148831da05e54
babu <bguerrero825@gmail.com>
2026-04-17 13:43:47 -0500

    added new binaries not in apt, helix config stuff too

--- dodots.fish
+++ dodots.fish
@@ -1,26 +1,27 @@
 #!/usr/bin/fish
 
-if [ $argv[1] = "push" ] 
-	cd ~/dev/moredots
-	git fetch
-	git pull
-	rsync -avz ~/.tmux.conf ~/.config/kitty ~/.config/fish ~/.local/share/fonts ~/dev/moredots ;
-	git add -u
-	git commit -m (hostname)": more dots!"
-	git push
-	echo "[+] dots pushed!"
+if [ $argv[1] = push ]
+    cd ~/dev/moredots
+    git fetch
+    git pull
+    rsync -avz ~/.tmux.conf ~/.config/kitty ~/.config/fish ~/.config/helix ~/.local/share/fonts ~/dev/moredots
+    git add -u
+    git commit -m (hostname)": more dots!"
+    git push
+    echo "[+] dots pushed!"
 
-else if [ $argv[1] = "pull" ]
-	cd ~/dev/moredots
-	git fetch
-	git pull
-	rsync -avz ~/dev/moredots/.tmux.conf ~/.tmux.conf ;
-	rsync -avz ~/dev/moredots/fish ~/.config/ ;
-	rsync -avz ~/dev/moredots/kitty ~/.config/ ;
-	rsync -avz ~/dev/moredots/fonts ~/.local/share/ ;
-	echo "[+] dots pulled!"
+else if [ $argv[1] = pull ]
+    cd ~/dev/moredots
+    git fetch
+    git pull
+    rsync -avz ~/dev/moredots/.tmux.conf ~/.tmux.conf
+    rsync -avz ~/dev/moredots/fish ~/.config/
+    rsync -avz ~/dev/moredots/kitty ~/.config/
+    rsync -avz ~/dev/moredots/helix ~/.config/
+    rsync -avz ~/dev/moredots/fonts ~/.local/share/
+    echo "[+] dots pulled!"
 
 else
-	echo "Would you like to 'push' or 'pull' dots?"
+    echo "Would you like to 'push' or 'pull' dots?"
 
 end
--- setup.sh
+++ setup.sh
@@ -2,15 +2,23 @@
 
 # !! DO NOT run as root !!
 #
-# Assuming you got this script by setting up git and/or gh
-# Also assuming you're running this from the ~/dev/moredots directory
+# Assuming you got this script by already setting up git and/or gh
+mkdir ~/dev
+cd ~/dev
 
 # add current user to sudoers
 me=$(whoami)
 sudo echo -e "\n# No password sudo\n$me ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/usernopass 1>/dev/null
 
 # get stuff
-sudo apt install git fish kitty tmux gh gcc fzf
+sudo apt install git fish kitty tmux gh gcc fzf 
+# others TBD...
+
+# TODO untar and link included binaries
+# helix
+# zig
+# lsps? (zls)
+# others TBD...
 
 # make home directories
 mkdir ~/gamedev
@@ -19,6 +27,15 @@ mkdir ~/notes
 # pull and put dots in .config locations
 fish ~/dev/moredots/dodots.fish pull
 
+# set git identity
+git config --global user.name "babu"
+git config --global user.email "bguerrero825@gmail.com"
+
+# nano default GTFO
+sudo update-alternatives --config editor
+ 
+
+# ------------- nvim stuff I might delete
 # put nvim 0.11 in /usr/local dirs
 sudo chown -R $me:$me /usr/local
 cp -R ~/dev/moredots/nvim_0.11/bin/nvim /usr/local/bin/nvim
@@ -26,14 +43,6 @@ cp -R ~/dev/moredots/nvim_0.11/lib/nvim /usr/local/lib/nvim
 cp -R ~/dev/moredots/nvim_0.11/share/nvim /usr/local/share/nvim
 # symlink vim to nvim adjacently
 ln -s /usr/local/bin/nvim /usr/local/bin/vim
-
 # pull and set my nvim kickstart config
 git clone https://github.com/BGuerrero825/kickstart.nvim.git ~/.config/nvim
-
-# set git identity
-git config --global user.name "babu"
-git config --global user.email "bguerrero825@gmail.com"
-
-# nano default GTFO
-sudo update-alternatives --config editor
-
+# -------------
